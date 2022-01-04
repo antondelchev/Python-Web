dd050111564fc2b64896e7a33ecebd178ed5f55d
antondelchev <dlchv.mail@gmail.com>
2022-01-04 11:14:27 +0200

    Update.

--- todos_app/todos_app/todos/models.py
+++ todos_app/todos_app/todos/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
 
+class Person(models.Model):
+    name = models.CharField(max_length=28)
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=20)
+
+
 class Todo(models.Model):
     text = models.CharField(max_length=28)
     state = models.BooleanField(default=False)
-    completed_by = models.CharField(max_length=38, null=True)
+    owner = models.ForeignKey(
+        Person,
+        on_delete=models.CASCADE,
+        default=True,
+    )
+    category = models.ManyToManyField(Categories)
