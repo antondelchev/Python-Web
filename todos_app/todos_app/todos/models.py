894b01c6e515779479001787073aca25da8ea40e
antondelchev <dlchv.mail@gmail.com>
2022-01-05 23:06:01 +0200

    Update.

--- todos_app/todos_app/todos/models.py
+++ todos_app/todos_app/todos/models.py
@@ -4,17 +4,40 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=28)
 
+    def __str__(self):
+        return self.name
 
-class Categories(models.Model):
-    name = models.CharField(max_length=20)
+    class Meta:
+        verbose_name_plural = 'people'
+
+
+class Category(models.Model):
+    CHOICE_HOME = 'Home'
+    CHOICE_WORK = 'Work'
+    NAME_CHOICES = (
+        (CHOICE_HOME, '-- Home'),
+        (CHOICE_WORK, '-- Work'),
+    )
+
+    name = models.CharField(
+        max_length=20,
+        choices=NAME_CHOICES
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'categories'
 
 
 class Todo(models.Model):
     text = models.CharField(max_length=28)
     state = models.BooleanField(default=False)
+    description = models.TextField(null=True, blank=True)
     owner = models.ForeignKey(
         Person,
         on_delete=models.CASCADE,
         default=True,
     )
-    category = models.ManyToManyField(Categories)
+    category = models.ManyToManyField(Category)
