433903d3172a5994f278fb8e10eed61ac32c6998
antondelchev <dlchv.mail@gmail.com>
2022-02-10 21:52:49 +0200

    Update.

--- employees_app/employees_app/employees/views.py
+++ employees_app/employees_app/employees/views.py
@@ -1,5 +1,6 @@
 import random
 
+from django import forms
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
 
@@ -20,54 +21,65 @@ from django.shortcuts import render, redirect
 from employees_app.employees.models import Department, Employee
 
 
+class EmployeeForm(forms.Form):
+    first_name = forms.CharField(
+        max_length=30,
+    )
+
+    last_name = forms.CharField(
+        max_length=40,
+    )
+
+    age = forms.IntegerField()
+
+
 def home(request):
     context = {
-        'number': random.randint(0, 100),
-        'numbers': [random.randint(0, 100) for _ in range(3)],
+        'employee_form': EmployeeForm(),
     }
 
     return render(request, 'index.html', context)
 
-
-def not_found(request):
-    return HttpResponseNotFound()
-    # or --> raise Http404()
-
-
-def go_to_home(request):
-    # return redirect(to='/') <--- hardcoded path
-    # return redirect(to='index')
-    # ^ path with named url (mapped path url to view)
-
-    return redirect('department details', id=random.randint(1, 9))
-
-
-def department_details(request, id):
-    if not isinstance(id, int):
-        pass
-
-    return HttpResponse(f'This is department {id}.')
-
-
-def create_department(request):
-    return HttpResponse('Created')
-
-
-def list_departments(request):
-    # create new department on refresh/entering page:
-    # OPTION 1
-    department = Department(
-        name=f'Department {random.randint(1, 999)}',
-    )
-    department.save()  # <---- saves to the DB, thus creating the object
-    # OPTION 2
-    Department.objects.create(
-        name=f'Department {random.randint(1, 999)}',
-    )  # <--- saves automatically
-
-    context = {
-        'departments': Department.objects.prefetch_related('employee_set').all(),
-        'employees': Employee.objects.all(),
-        # 'employees': Employee.objects.filter(first_name__exact='Thomas'),
-    }
-    return render(request, 'list_departments.html', context)
+#
+# def not_found(request):
+#     return HttpResponseNotFound()
+#     # or --> raise Http404()
+#
+#
+# def go_to_home(request):
+#     # return redirect(to='/') <--- hardcoded path
+#     # return redirect(to='index')
+#     # ^ path with named url (mapped path url to view)
+#
+#     return redirect('department details', id=random.randint(1, 9))
+#
+#
+# def department_details(request, id):
+#     if not isinstance(id, int):
+#         pass
+#
+#     return HttpResponse(f'This is department {id}.')
+#
+#
+# def create_department(request):
+#     return HttpResponse('Created')
+#
+#
+# def list_departments(request):
+#     # create new department on refresh/entering page:
+#     # OPTION 1
+#     department = Department(
+#         name=f'Department {random.randint(1, 999)}',
+#     )
+#     department.save()  # <---- saves to the DB, thus creating the object
+#     # OPTION 2
+#     Department.objects.create(
+#         name=f'Department {random.randint(1, 999)}',
+#     )  # <--- saves automatically
+#
+#     context = {
+#         'departments': Department.objects.prefetch_related('employee_set').all(),
+#         'employees': Employee.objects.all(),
+#         # 'employees': Employee.objects.filter(first_name__exact='Thomas'),
+#     }
+#     return render(request, 'list_departments.html', context)
--- employees_app/employees_app/urls.py
+++ employees_app/employees_app/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from employees_app.employees.views import home, go_to_home
+from employees_app.employees.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='index'),
-    path('go-to-home/', go_to_home, name='go to home'),
-    path('departments/', include('employees_app.employees.urls')),
-    path('templates/', include('employees_app.template_examples.urls')),
+    # path('go-to-home/', go_to_home, name='go to home'),
+    # path('departments/', include('employees_app.employees.urls')),
+    # path('templates/', include('employees_app.template_examples.urls')),
 ]
