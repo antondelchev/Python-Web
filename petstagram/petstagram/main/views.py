54b39fd3181292205544f72a06778feeb83496c5
antondelchev <dlchv.mail@gmail.com>
2022-03-13 21:40:33 +0200

    Update.

--- petstagram/petstagram/main/views.py
+++ petstagram/petstagram/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from petstagram.main.models import Profile, PetPhoto
@@ -21,7 +21,9 @@ def show_home(request):
 
 def show_dashboard(request):
     profile = get_profile()
-    pet_photos = PetPhoto.objects.filter(tagged_pets__user_profile=profile)
+    pet_photos = set(PetPhoto.objects
+                     .prefetch_related('tagged_pets')
+                     .filter(tagged_pets__user_profile=profile))
     context = {
         'pet_photos': pet_photos,
     }
@@ -30,8 +32,25 @@ def show_dashboard(request):
 
 
 def show_profile(request):
-    return render(request, 'profile_details.html')
+    context = {
+        'profile': get_profile(),
+    }
+    return render(request, 'profile_details.html', context)
 
 
 def show_pet_photo_details(request, pk):
-    return render(request, 'photo_details.html')
+    pet_photo = PetPhoto.objects.prefetch_related('tagged_pets').get(pk=pk)
+
+    context = {
+        'pet_photo': pet_photo,
+    }
+
+    return render(request, 'photo_details.html', context)
+
+
+def like_pet_photo(request, pk):
+    pet_photo = PetPhoto.objects.get(pk=pk)
+    pet_photo.likes += 1
+    pet_photo.save()
+
+    return redirect('pet_photo_details', pk)
