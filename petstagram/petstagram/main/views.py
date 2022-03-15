6700e5c29e824fe27b30c1ba154ef3fdcb1bef24
antondelchev <dlchv.mail@gmail.com>
2022-03-15 21:51:42 +0200

    Update.

--- petstagram/petstagram/main/views.py
+++ petstagram/petstagram/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from petstagram.main.models import Profile, PetPhoto
+from petstagram.main.models import Profile, PetPhoto, Pet
 
 
 def get_profile():
@@ -33,20 +33,19 @@ def show_dashboard(request):
 
 def show_profile(request):
     profile = get_profile()
-    total_likes_count = sum(pp.likes for pp in PetPhoto.objects.
-                            filter(tagged_pets__user_profile=profile).
-                            distinct()
-                            )
-    total_pet_photos_count = len(PetPhoto.objects
-                                 .filter(tagged_pets__user_profile=profile)
-                                 .distinct()
-                                 )
+    
+    pets = Pet.objects.filter(user_profile=profile)
+    pet_photos = PetPhoto.objects.filter(tagged_pets__in=pets).distinct()
+
+    total_likes_count = sum(pp.likes for pp in pet_photos)
+    total_pet_photos_count = len(pet_photos)
 
     context = {
         'profile': profile,
         'total_likes_count': total_likes_count,
         'total_pet_photos_count': total_pet_photos_count,
     }
+
     return render(request, 'profile_details.html', context)
 
 
