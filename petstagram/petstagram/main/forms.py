e736b557ac35110e0c86d614ed28b4b33873be32
antondelchev <dlchv.mail@gmail.com>
2022-03-25 21:52:55 +0200

    Update.

--- petstagram/petstagram/main/forms.py
+++ petstagram/petstagram/main/forms.py
@@ -7,3 +7,23 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ('first_name', 'last_name', 'picture')
+        widgets = {
+            'first_name': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Enter first name',
+                }
+            ),
+            'last_name': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Enter last name',
+                }
+            ),
+            'picture': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Enter URL',
+                }
+            ),
+        }
