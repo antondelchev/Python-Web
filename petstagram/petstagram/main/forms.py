30737ba104a7fcc06306bb927d43c48c72486210
antondelchev <dlchv.mail@gmail.com>
2022-03-27 22:22:30 +0300

    Update.

--- petstagram/petstagram/main/forms.py
+++ petstagram/petstagram/main/forms.py
@@ -1,28 +1,30 @@
 from django import forms
 
+from petstagram.main.helpers import BootstrapFormMixin
 from petstagram.main.models import Profile
 
 
-class ProfileForm(forms.ModelForm):
+class ProfileForm(BootstrapFormMixin, forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._init_bootstrap_form_controls()
+
     class Meta:
         model = Profile
         fields = ('first_name', 'last_name', 'picture')
         widgets = {
             'first_name': forms.TextInput(
                 attrs={
-                    'class': 'form-control',
                     'placeholder': 'Enter first name',
                 }
             ),
             'last_name': forms.TextInput(
                 attrs={
-                    'class': 'form-control',
                     'placeholder': 'Enter last name',
                 }
             ),
             'picture': forms.TextInput(
                 attrs={
-                    'class': 'form-control',
                     'placeholder': 'Enter URL',
                 }
             ),
--- petstagram/petstagram/main/helpers.py
+++ petstagram/petstagram/main/helpers.py
@@ -7,3 +7,14 @@ def get_profile():
         return profiles[0]
 
     return None
+
+
+class BootstrapFormMixin:
+    fields = {}
+
+    def _init_bootstrap_form_controls(self):
+        for _, fields in self.fields.items():
+            if 'class' not in fields.widget.attrs:
+                fields.widget.attrs['class'] = ''
+
+            fields.widget.attrs['class'] += ' form-control'
