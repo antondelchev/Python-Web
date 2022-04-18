4875056ca57b0e132686b006127f9f7ebd969323
antondelchev <dlchv.mail@gmail.com>
2022-04-18 11:56:31 +0300

    Update.

--- petstagram/petstagram/main/forms.py
+++ petstagram/petstagram/main/forms.py
@@ -1,4 +1,8 @@
+import datetime
+from datetime import date
+
 from django import forms
+from django.core.exceptions import ValidationError
 
 from petstagram.main.helpers import BootstrapFormMixin, DisableFieldsFormMixin
 from petstagram.main.models import Profile, PetPhoto, Pet
@@ -109,9 +113,18 @@ class CreatePetForm(BootstrapFormMixin, forms.ModelForm):
 
 
 class EditPetForm(BootstrapFormMixin, forms.ModelForm):
+    MIN_DATE_OF_BIRTH = date(1920, 1, 1)
+    MAX_DATE_OF_BIRTH = date.today()
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._init_bootstrap_form_controls()
+
+    def clean_date_of_birth(self):
+        date_of_birth = self.cleaned_data['date_of_birth']
+        if date_of_birth < self.MIN_DATE_OF_BIRTH or self.MAX_DATE_OF_BIRTH < date_of_birth:
+            raise ValidationError(f'Date must be between {self.MIN_DATE_OF_BIRTH} and {self.MAX_DATE_OF_BIRTH}')
+        return date_of_birth
 
     class Meta:
         model = Pet
