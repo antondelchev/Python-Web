0f1263f16c88cca3a2ce4219ace470afb73ddc28
antondelchev <dlchv.mail@gmail.com>
2022-04-20 21:47:05 +0300

    Update.

--- petstagram/petstagram/main/validators.py
+++ petstagram/petstagram/main/validators.py
@@ -27,3 +27,12 @@ class ValidateFileMaxSizeInMb:
 
     def __call__(self, value):
         pass
+
+
+class MinDateValidator:
+    def __init__(self, min_date):
+        self.min_date = min_date
+
+    def __call__(self, value):
+        if value < self.min_date:
+            raise ValidationError(f'Date must be greater than {self.min_date}')
