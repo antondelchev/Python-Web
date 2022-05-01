372431d9fb4a8f7cfffb5cb06ab217a15f9bac44
antondelchev <dlchv.mail@gmail.com>
2022-05-01 21:40:11 +0300

    Update.

--- expenses_tracker/expenses_tracker/settings.py
+++ expenses_tracker/expenses_tracker/settings.py
@@ -13,6 +13,8 @@ from os import getenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from expenses_tracker.access_info import password
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'expenses_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': password,
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
