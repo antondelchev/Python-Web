7648460e54caa26058933de5663f1efaf19a3567
antondelchev <dlchv.mail@gmail.com>
2022-05-06 22:46:22 +0300

    Update.

--- expenses_tracker/expenses_tracker/web/urls.py
+++ expenses_tracker/expenses_tracker/web/urls.py
@@ -1,1 +1,13 @@
-urlpatterns = []
+from django.urls import path
+
+urlpatterns = [
+    path('', show_index, name='show index'),
+
+    path('create/', create_expense, name='create expense'),
+    path('edit/<id>/', edit_expense, name='edit expense'),
+    path('delete/<id>', delete_expense, name='delete expense'),
+
+    path('profile/', show_profile, name='show profile'),
+    path('profile/edit/', edit_profile, name='edit profile'),
+    path('profile/delete/', delete_profile, name='delete profile'),
+]
