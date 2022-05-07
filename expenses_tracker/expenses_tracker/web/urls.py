9fd86b96c6f9c4e381af0ac02976e14a49b4028a
antondelchev <dlchv.mail@gmail.com>
2022-05-07 11:48:27 +0300

    Update.

--- expenses_tracker/expenses_tracker/web/urls.py
+++ expenses_tracker/expenses_tracker/web/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from expenses_tracker.web.views import show_index, create_expense, edit_expense, delete_expense, show_profile, \
+    edit_profile, delete_profile
+
 urlpatterns = [
     path('', show_index, name='show index'),
 
