df53534211ab5eef14da07fdac26f0f1b80145ae
antondelchev <dlchv.mail@gmail.com>
2022-05-14 22:57:34 +0300

    Update.

--- expenses_tracker/expenses_tracker/web/urls.py
+++ expenses_tracker/expenses_tracker/web/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from expenses_tracker.web.views import show_index, create_expense, edit_expense, delete_expense, show_profile, \
-    edit_profile, delete_profile
+    edit_profile, delete_profile, create_profile
 
 urlpatterns = [
     path('', show_index, name='show index'),
@@ -11,6 +11,7 @@ urlpatterns = [
     path('delete/<int:pk>', delete_expense, name='delete expense'),
 
     path('profile/', show_profile, name='show profile'),
+    path('profile/create/', create_profile, name='create profile'),
     path('profile/edit/', edit_profile, name='edit profile'),
     path('profile/delete/', delete_profile, name='delete profile'),
 ]
