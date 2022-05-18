cee89d9af1839d98d859c6efe69e45670c753e0d
antondelchev <dlchv.mail@gmail.com>
2022-05-18 23:14:54 +0300

    Update.

--- expenses_tracker/expenses_tracker/web/models.py
+++ expenses_tracker/expenses_tracker/web/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Profile(models.Model):
+    first_name = models.CharField(
+
+    )
+
+    last_name = models.CharField(
+
+    )
+
+    budget = models.FloatField(
+
+    )
+
+    image = models.ImageField(
+        
+    )
+
+
+class Expense(models.Model):
+    pass
