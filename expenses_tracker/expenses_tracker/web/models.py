fd485556546457bda2fb0517f7f1b2f728d85c84
antondelchev <dlchv.mail@gmail.com>
2022-05-19 23:17:37 +0300

    Update.

--- expenses_tracker/expenses_tracker/web/models.py
+++ expenses_tracker/expenses_tracker/web/models.py
@@ -1,23 +1,60 @@
+from django.core.validators import MinLengthValidator, MinValueValidator
 from django.db import models
 
 
 # Create your models here.
 
-class Profile(models.Model):
-    first_name = models.CharField(
+def validate_only_letters(value):
+    pass
 
+
+class MaxFileSizeInMbValidator:
+    pass
+
+
+class Profile(models.Model):
+    FIRST_NAME_MIN_LEN = 2
+    FIRST_NAME_MAX_LEN = 15
+
+    LAST_NAME_MIN_LEN = 2
+    LAST_NAME_MAX_LEN = 15
+
+    BUDGET_DEFAULT_VALUE = 0
+    BUDGET_MIN_VALUE = 0
+
+    IMAGE_MAX_SIZE_IN_MB = 5
+    IMAGE_UPLOAD_TO_DIR = 'profiles/'
+
+    first_name = models.CharField(
+        max_length=FIRST_NAME_MAX_LEN,
+        validators=(
+            MinLengthValidator(FIRST_NAME_MIN_LEN),
+            validate_only_letters,
+        ),
     )
 
     last_name = models.CharField(
-
+        max_length=LAST_NAME_MAX_LEN,
+        validators=(
+            MinLengthValidator(LAST_NAME_MIN_LEN),
+            validate_only_letters,
+        ),
     )
 
     budget = models.FloatField(
-
+        default=BUDGET_DEFAULT_VALUE,
+        validators=(
+            MinValueValidator(BUDGET_MIN_VALUE),
+        ),
     )
 
     image = models.ImageField(
-        
+        upload_to=IMAGE_UPLOAD_TO_DIR,
+        null=True,
+        blank=True,
+        validators=(
+            MaxFileSizeInMbValidator(IMAGE_MAX_SIZE_IN_MB),
+        ),
     )
 
 
